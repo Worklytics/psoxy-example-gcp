ace0cc205e17833c0d0f70043dae2b9295ba2d02
Erik Schultink <erik@worklytics.co>
2023-08-16 14:18:27 -0700

    Update example to v0.4.35

--- main.tf
+++ main.tf
@@ -39,16 +39,18 @@ locals {
 # be provisioned via Terraform, so doesn't add any dependencies
 # call this 'generic_source_connectors'?
 module "worklytics_connectors" {
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors?ref=v0.4.31"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors?ref=v0.4.35"
 
-  enabled_connectors        = var.enabled_connectors
-  jira_cloud_id             = var.jira_cloud_id
-  jira_server_url           = var.jira_server_url
-  jira_example_issue_id     = var.jira_example_issue_id
-  salesforce_domain         = var.salesforce_domain
-  github_installation_id    = var.github_installation_id
-  github_organization       = var.github_organization
-  github_example_repository = var.github_example_repository
+
+  enabled_connectors            = var.enabled_connectors
+  jira_cloud_id                 = var.jira_cloud_id
+  jira_server_url               = var.jira_server_url
+  jira_example_issue_id         = var.jira_example_issue_id
+  salesforce_domain             = var.salesforce_domain
+  github_installation_id        = var.github_installation_id
+  github_organization           = var.github_organization
+  github_example_repository     = var.github_example_repository
+  salesforce_example_account_id = var.salesforce_example_account_id
 }
 
 # sources which require additional dependencies are split into distinct Terraform files, following
@@ -85,32 +87,33 @@ locals {
 }
 
 module "psoxy" {
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/gcp-host?ref=v0.4.31"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/gcp-host?ref=v0.4.35"
 
-  gcp_project_id                 = var.gcp_project_id
-  environment_name               = var.environment_name
-  config_parameter_prefix        = var.config_parameter_prefix
-  default_labels                 = var.default_labels
-  worklytics_sa_emails           = var.worklytics_sa_emails
-  psoxy_base_dir                 = var.psoxy_base_dir
-  deployment_bundle              = var.deployment_bundle
-  force_bundle                   = var.force_bundle
-  install_test_tool              = var.install_test_tool
-  gcp_region                     = var.gcp_region
-  replica_regions                = coalesce(var.replica_regions, var.gcp_secret_replica_locations)
-  api_connectors                 = local.api_connectors
-  bulk_connectors                = local.bulk_connectors
-  non_production_connectors      = var.non_production_connectors
-  custom_api_connector_rules     = var.custom_api_connector_rules
-  general_environment_variables  = var.general_environment_variables
-  pseudonymize_app_ids           = var.pseudonymize_app_ids
-  bulk_input_expiration_days     = var.bulk_input_expiration_days
-  bulk_sanitized_expiration_days = var.bulk_sanitized_expiration_days
-  custom_bulk_connector_rules    = var.custom_bulk_connector_rules
-  lookup_tables                  = var.lookup_tables
-  custom_artifacts_bucket_name   = var.custom_artifacts_bucket_name
-  todos_as_local_files           = var.todos_as_local_files
-  todo_step                      = local.max_auth_todo_step
+  gcp_project_id                  = var.gcp_project_id
+  environment_name                = var.environment_name
+  config_parameter_prefix         = var.config_parameter_prefix
+  default_labels                  = var.default_labels
+  worklytics_sa_emails            = var.worklytics_sa_emails
+  psoxy_base_dir                  = var.psoxy_base_dir
+  deployment_bundle               = var.deployment_bundle
+  force_bundle                    = var.force_bundle
+  install_test_tool               = var.install_test_tool
+  gcp_region                      = var.gcp_region
+  replica_regions                 = coalesce(var.replica_regions, var.gcp_secret_replica_locations)
+  api_connectors                  = local.api_connectors
+  bulk_connectors                 = local.bulk_connectors
+  non_production_connectors       = var.non_production_connectors
+  custom_api_connector_rules      = var.custom_api_connector_rules
+  general_environment_variables   = var.general_environment_variables
+  pseudonymize_app_ids            = var.pseudonymize_app_ids
+  bulk_input_expiration_days      = var.bulk_input_expiration_days
+  bulk_sanitized_expiration_days  = var.bulk_sanitized_expiration_days
+  custom_bulk_connector_rules     = var.custom_bulk_connector_rules
+  custom_bulk_connector_arguments = var.custom_bulk_connector_arguments
+  lookup_tables                   = var.lookup_tables
+  custom_artifacts_bucket_name    = var.custom_artifacts_bucket_name
+  todos_as_local_files            = var.todos_as_local_files
+  todo_step                       = local.max_auth_todo_step
 }
 
 locals {
@@ -121,8 +124,7 @@ locals {
 module "connection_in_worklytics" {
   for_each = local.all_instances
 
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-psoxy-connection-generic?ref=v0.4.31"
-
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-psoxy-connection-generic?ref=v0.4.35"
 
   psoxy_host_platform_id = local.host_platform_id
   psoxy_instance_id      = each.key
@@ -167,15 +169,12 @@ output "todos_3" {
   value       = var.todos_as_outputs ? join("\n", values(module.connection_in_worklytics)[*].todo) : null
 }
 
-moved {
-  from = module.psoxy.module.secrets["jira-cloud"].google_secret_manager_secret.secret["JIRA_CLOUD_REFRESH_TOKEN"]
-  to   = module.psoxy.module.secrets["jira-cloud"].google_secret_manager_secret.secret["REFRESH_TOKEN"]
-}
-moved {
-  from = module.psoxy.module.secrets["jira-cloud"].google_secret_manager_secret_version.version["JIRA_CLOUD_REFRESH_TOKEN"]
-  to   = module.psoxy.module.secrets["jira-cloud"].google_secret_manager_secret_version.version["REFRESH_TOKEN"]
-}
-moved {
-  from = module.psoxy.module.api_connector["jira-cloud"].google_secret_manager_secret_iam_member.grant_sa_accessor_on_secret["JIRA_CLOUD_REFRESH_TOKEN"]
-  to   = module.psoxy.module.api_connector["jira-cloud"].google_secret_manager_secret_iam_member.grant_sa_accessor_on_secret["REFRESH_TOKEN"]
-}
+# although should be sensitive such that Terraform won't echo it to command line or expose it, leave
+# commented out in example until needed
+# if you uncomment it, you will then be able to obtain the value through `terraform output --raw pseudonym_salt`
+#output "pseudonym_salt" {
+#  description = "Value used to salt pseudonyms (SHA-256) hashes. If migrate to new deployment, you should copy this value."
+#  value       = module.psoxy.pseudonym_salt
+#  sensitive   = true
+#}
+
