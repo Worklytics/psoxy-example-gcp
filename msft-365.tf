fd9964a52e7744af74897f866f6a92085ab720ca
Erik Schultink <eschultink@users.noreply.github.com>
2026-01-29 12:29:36 -0800

    Update example to v0.5.16

--- msft-365.tf
+++ msft-365.tf
@@ -1,7 +1,7 @@
 # BEGIN MSFT
 
 module "worklytics_connectors_msft_365" {
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors-msft-365?ref=v0.5.15"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors-msft-365?ref=v0.5.16"
 
   enabled_connectors                         = var.enabled_connectors
   environment_id                             = var.environment_name
@@ -32,7 +32,7 @@ locals {
 module "msft-connection-auth-federation" {
   for_each = module.worklytics_connectors_msft_365.enabled_api_connectors
 
-  source = "git::https://github.com/worklytics/psoxy//infra/modules/azuread-federated-credentials?ref=v0.5.15"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/azuread-federated-credentials?ref=v0.5.16"
 
   application_id = each.value.connector.id
   display_name   = "GcpFederation"
